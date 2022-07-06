e2c2249d0a14862f8e1c5a4236574c7ebea2b57d
David Taccetta <taccetta14@gmail.com>
2022-07-06 10:25:58 -0400

    Add project files.

---
+++ WeatherForcast/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherForcast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var weatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=30.4383&lon=84.2807&appid=39626552851cd3ee9afbc77157d6b50e&units=imperial";
+
+            var weather = client.GetStringAsync(weatherURL).Result;
+
+            Console.WriteLine(weather);
+        }
+    }
+}
